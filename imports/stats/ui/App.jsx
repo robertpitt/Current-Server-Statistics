70175c272fddd931d8a42f13ed6b7f5a5336cafe
Jake Lacey <jakewlacey@gmail.com>
2016-06-13 20:58:44 +0100

    JL: Adding boilerplate chart

--- imports/stats/ui/App.jsx
+++ imports/stats/ui/App.jsx
@@ -8,15 +8,39 @@ import React, { Component, PropTypes } from 'react';
 // theses are the subcompoenents we are pulling in.
 import Header from './components/header/Header.jsx';
 
+// adding line...
+var LineChart = require("react-chartjs").Line;
+
 //App component - reporesents the entire application from the top level
 export default class App extends Component {
   render() {
+
+    var chartData = {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+          {
+              label: "My First dataset",
+              responsive: true,
+              fillColor: "rgba(0,220,220,0.5)",
+              data: [65, 59, 80, 81, 56, 55, 100],
+          }
+      ]
+    }
+
+    var chartOptions = {
+      title: {
+           display: true,
+           text: 'Custom Chart Title'
+       },
+       responsive: true
+    }
+
     return (
       <div className="container">
         <Header />
 
         <div className="container">
-          this is the whole application.
+          <LineChart data={chartData} options={chartOptions} />
         </div>
       </div>
     )
--- server/main.js
+++ server/main.js
@@ -1,8 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 
-// this is example of making a collection..
-import Posts from '../imports/example/app/api/posts.js';  // delete
-
 Meteor.startup(() => {
   // code to run on server at startup
 });
